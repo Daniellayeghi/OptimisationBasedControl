7df2487f39ddd058c6076ccdbaabf7cb0fde78f5
Daniellayeghi <Daniellayeghi@gmail.com>
2020-02-17 18:01:21 +0000

    Added derivative features

--- src/controller/controller.cpp
+++ src/controller/controller.cpp
@@ -5,7 +5,6 @@
 
 static MyController *my_ctrl;
 
-// C++ includes
 #include <iostream>
 #include "autodiff/forward.hpp"
 
@@ -27,27 +26,11 @@ MyController::MyController(const mjModel *m, mjData *d) : _m(m), _d(d)
     {
         _constant_acc[row] = 0.4;
     }
-
-    dual x = 1.0;  // the input variable x
-    dual y = 2.0;  // the input variable y
-    dual z = 3.0;  // the input variable z
-
-    dual u = f(x, y, z);  // the output scalar u = f(x, y, z)
-
-    double dudx = derivative(f, wrt(x), at(x, y, z));  // evaluate du/dx
-    double dudy = derivative(f, wrt(y), at(x, y, z));  // evaluate du/dy
-    double dudz = derivative(f, wrt(z), at(x, y, z));  // evaluate du/dz
-
-    cout << "u = " << u << endl;         // print the evaluated output u
-    cout << "du/dx = " << dudx << endl;  // print the evaluated derivative du/dx
-    cout << "du/dy = " << dudy << endl;  // print the evaluated derivative du/dy
-    cout << "du/dz = " << dudz << endl;  // print the evaluated derivative du/dz
 }
 
 
 void MyController::controller()
 {
-
     dual x = 1.0;
     dual y = 2.0;
     dual z = 3.0;
@@ -64,8 +47,6 @@ void MyController::controller()
     cout << "du/dz = " << dudz << endl;
 
     mj_mulM(_m, _d, _inertial_torque, _constant_acc);
-
-//    std::cout << _d->qfrc_bias[0] << " " << _d->qfrc_bias[1] << " " <<_d->qfrc_bias[2] << std::endl;
 
     _d->qfrc_applied[0] = _d->qfrc_bias[0] + _inertial_torque[0];
     _d->qfrc_applied[1] = _d->qfrc_bias[1] + _inertial_torque[0];
