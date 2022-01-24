14c2a6a6ef2755c80b9186c24c023071021c4b24
Daniellayeghi <Daniellayeghi@gmail.com>
2022-01-24 16:25:59 +0000

    Change time

--- app/tasks/mppi_ilqr_cartpole.cpp
+++ app/tasks/mppi_ilqr_cartpole.cpp
@@ -224,7 +224,7 @@ int main(int argc, const char** argv)
 
         // To show difference in sampling try 3 samples
         MPPIDDPParams params{
-            30, 45, 0.2, 1, 1, 1, 1000,ctrl_mean,
+            4, 75, 0.01, 1, 1, 1, 1000,ctrl_mean,
             ddp_var, ctrl_var, {ilqr.m_u_traj_cp, ilqr._covariance}, seed
         };
         QRCostDDP qrcost(params, running_cost, terminal_cost);
